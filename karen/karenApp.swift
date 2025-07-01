4bb939e8f41dde54466af5d6677b1b938e96e3ef
George <george@arrowsmith.codes>
2025-07-01 11:32:37 +0100

    Initial Commit

---
+++ karen/karenApp.swift
@@ -0,0 +1,17 @@
+//
+//  karenApp.swift
+//  karen
+//
+//  Created by George Millo on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct karenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ karenTests/karenTests.swift
@@ -0,0 +1,17 @@
+//
+//  karenTests.swift
+//  karenTests
+//
+//  Created by George Millo on 01/07/2025.
+//
+
+import Testing
+@testable import karen
+
+struct karenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
